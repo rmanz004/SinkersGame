f620281821061cc9ba2ab878651ca16f97eaba2c
Aaron Kim <32529743+AaronKimCE@users.noreply.github.com>
2021-04-23 10:01:29 -0700

    Collision Damage v1

--- Sinker/Assets/Scripts/Bullet.cs
+++ Sinker/Assets/Scripts/Bullet.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     public float speed = 20f;
     public Rigidbody2D rb;
+    [SerializeField] private PlayerHealth health;
     void Start()
     {
         rb.velocity = transform.right * speed;
     }
 
     void OnTriggerEnter2D(Collider2D hitInfo){
-                
+        if (hitInfo.CompareTag("Player"))
+        {
+            health.DamagePlayer(1);
+        }
+
         Destroy(gameObject);
     }
 
